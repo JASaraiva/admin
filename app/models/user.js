837776290bea7d31c450907167b2eb153c3a94f1
Jonas Almeida <jo.almeida@reply.com>
2020-09-21 12:17:24 -0300

    implementacao modulo user

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,60 @@
-module.exports = function(){
+
+const mongoose = require('mongoose');
+
+module.exports = function(app){
+
+    this.getUsers = function(){
+        const expDB = app.config.dbConnection();
+        expDB.on('error', console.error.bind(console, 'connection error:'));
+ 
+        expDB.once('open', function() {
+            console.log("Connection Successful!");
+           // define Schema
+           var UserSchema = mongoose.Schema({
+            name: String,
+            username: String,
+            password: String,
+            email: String
+          });
+        
+          var User = mongoose.model('User', UserSchema, 'user');
+      
+          var users =  User.find();
+
+          console.log('---' + users);
+      
+        });
+    }
+
+
+    this.setUser = function(data){
+        const expDB = app.config.dbConnection();
+        expDB.on('error', console.error.bind(console, 'connection error:'));
+ 
+        expDB.once('open', function() {
+          console.log("Connection Successful!");
+        
+          // define Schema
+          var UserSchema = mongoose.Schema({
+            name: String,
+            username: String,
+            password: String,
+            email: String
+          });
     
-    this.getUsers = function(connection, callback){
-        connection.query('SELECT * FROM users', callback)
+          // compile schema to model
+          var User = mongoose.model('User', UserSchema, 'user');
+      
+          // a document instance
+          var user1 = new User({ name: data.name, username: data.username, password: data.password, email : data.email });
+      
+          // save model to database
+          user1.save(function (err, user) {
+            if (err){
+                console.log(err);
+            }
+          });
+        });
     }
 
     return this;
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,44 +1,19 @@
-const dbConnection = require("../../config/dbConnection");
-const mongoose = require('mongoose');
+module.exports = function(application){
 
-module.exports = function(app){
 
-    app.get('/users', function(req, res){
-      res.send({msg: 'ola mundo'});
+  application.get('/users', function(req, res){
+      const userModel = application.app.models.user;
+
+      var users = userModel.getUsers()
+
+      res.send({msg: 'enviou', users: users});
     });
 
 
-    app.post('/users', function(req, res){
+    application.post('/users', function(req, res){
         var data = req.body;
-        const expDB = app.config.dbConnection();
-
-        expDB.on('error', console.error.bind(console, 'connection error:'));
- 
-        expDB.once('open', function() {
-          console.log("Connection Successful!");
-        
-          // define Schema
-          var UserSchema = mongoose.Schema({
-            name: String,
-            username: String,
-            password: String,
-            email: String
-          });
-    
-          // compile schema to model
-          var User = mongoose.model('User', UserSchema, 'apiadmin');
-      
-          // a document instance
-          var user1 = new User({ name: data.name, username: data.username, password: data.password, email : data.email });
-      
-          // save model to database
-          user1.save(function (err, user) {
-            if (err){
-              res.send("CUSTOM MSG ERROR: " +err);
-            }
-              res.send(user.name + " saved to users collection.");
-          });
-        });
+        const userModel = application.app.models.user;
+        var users = userModel.setUser();
     });
 
 }
